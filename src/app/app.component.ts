f6c77a0fe2bdd933d82baaf344ac3680ede63f27
sabarimalai_alagar <sabarimalai_alagar>
2025-08-05 21:06:02 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,16 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 declare var bootstrap: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-})
+  selector: 'app-root', 
+  templateUrl: './app.component.html',  
+}) 
 export class AppComponent {
   title = 'Coimbatore Sparks Rotaract';
   videoUrl: SafeResourceUrl;
 
   constructor(private sanitizer: DomSanitizer) {
     this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
-      'https://www.youtube.com/embed/A7PSoLRhkZg?autoplay=1'
+      'assets/videos/TeamIntro.mp4'
     );
   }
 
